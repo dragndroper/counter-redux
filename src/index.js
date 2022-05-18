c2af8752346e315423b9e234b1f65b46851a5cef
avel-front <inbox.kalashnikov@gmail.com>
2022-05-18 22:55:56 +0300

    added counter-redux

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import './styles.css'
+import {applyMiddleware, createStore} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+import {asyncIncrement, changeTheme, decrement, increment} from "./redux/actions";
+import thunk from "redux-thunk";
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme')
+
+// Подключение devtools
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk, logger)
+    )
+);
+
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment())
+})
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement())
+})
+
+asyncBtn.addEventListener('click', () => {
+    store.dispatch(asyncIncrement())
+})
+
+themeBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light')
+        ? 'dark' : 'light'
+    store.dispatch(changeTheme(newTheme))
+})
+
+
+store.subscribe(() => {
+    const state = store.getState()
+    counter.textContent = state.counter
+    document.body.className = state.theme.value;
+
+    [addBtn, subBtn, themeBtn, asyncBtn].forEach(btn => {
+        btn.disabled = state.theme.disabled
+    })
+})
+
+store.dispatch({type: 'INIT_APPLICATION'})
---
+++ src/redux/actions.js
@@ -0,0 +1,43 @@
+import {INCREMENT, DECREMENT, CHANGE_THEME, DISABLE_BUTTONS, ENABLE_BUTTONS} from "./types";
+
+export function increment() {
+    return {
+        type: INCREMENT
+    }
+}
+
+export function decrement() {
+    return {
+        type: DECREMENT
+    }
+}
+
+// redux-thunk. Async и disable для кнопки.
+export function asyncIncrement() {
+    return function (dispatch) {
+        dispatch(disableButtons())
+        setTimeout(() => {
+            dispatch(increment())
+            dispatch(enableButtons())
+        }, 1500)
+    }
+}
+
+export function changeTheme(newTheme) {
+    return {
+        type: CHANGE_THEME,
+        payload: newTheme
+    }
+}
+
+export function disableButtons() {
+    return {
+        type: DISABLE_BUTTONS
+    }
+}
+
+export function enableButtons() {
+    return {
+        type: ENABLE_BUTTONS
+    }
+}
